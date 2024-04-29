dba7ec74074db2ca4351a170c635a94d87d6b232
Liz Johnson <liz.jade.johnson@gmail.com>
2024-04-28 21:31:52 -0400

    passed props effectively, pages displaying correctly now

--- views/HoroscopeResult.jsx
+++ views/HoroscopeResult.jsx
@@ -14,33 +14,32 @@ function formatDate(date) {
     return new Intl.DateTimeFormat('en-US', options).format(dateObject);
 }
 
+class HoroscopeResult extends React.Component {
+    render() {
+        const { sign, dob, user } = this.props; // Destructure props here in render method
 
-function HoroscopeResult({ sign, dob }) {
-    if (!sign || !dob) {
-        return <Layout title="Error">Missing horoscope data!</Layout>;
+        if (!sign || !dob) {
+            return <Layout title="Error" user={user}>Missing horoscope data!</Layout>;
+        }
+
+        const startDateFormatted = formatDate(sign.startDate);
+        const endDateFormatted = formatDate(sign.endDate);
+        const gifPath = `/img/${sign.name.toLowerCase()}.gif`; // Ensure sign name is lowercase to match the file name
+
+        return (
+            <Layout title="Your Horoscope" user={user}>
+                <p>Sign: {sign.name}</p>
+                <p>Date Range: {startDateFormatted} - {endDateFormatted}</p>
+                <p>Element: {sign.element}</p>
+                <p>Quality: {sign.quality}</p>
+                <p>Ruling Planet: {sign.rulingPlanet}</p>
+                <p>Traits: {sign.traits.join(', ')}</p>
+                <p>Symbol: {sign.symbol}</p>
+                <p>Compatibility: {sign.compatibility.join(', ')}</p>
+                <img className="zodiacgif" src={gifPath} alt={`${sign.name} sign`} />
+            </Layout>
+        );
     }
-    
-    // Format start and end dates using the formatDate function
-    const startDateFormatted = formatDate(sign.startDate);
-    const endDateFormatted = formatDate(sign.endDate);
-
-    //Make a path to where our gif is located:
-    const gifPath = `/img/${sign.name.toLowerCase()}.gif`; //sign name has to be lowercase to match the file name
-
-
-    return (
-        <Layout title="Your Horoscope">
-            <p>Sign: {sign.name}</p>
-            <p>Date Range: {startDateFormatted} - {endDateFormatted}</p>
-            <p>Element: {sign.element}</p>
-            <p>Quality: {sign.quality}</p>
-            <p>Ruling Planet: {sign.rulingPlanet}</p>
-            <p>Traits: {sign.traits.join(', ')}</p>
-            <p>Symbol: {sign.symbol}</p>
-            <p>Compatibility: {sign.compatibility.join(', ')}</p>
-            <img className="zodiacgif" src={gifPath} alt={`${sign.name} sign`} />
-        </Layout>
-    );
 }
 
 module.exports = HoroscopeResult;
